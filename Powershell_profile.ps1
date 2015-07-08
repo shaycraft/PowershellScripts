8261966eda427da2dd5cf8faa5d83a5230080322
Sam <phreakdawg@gmail.com>
2015-07-08 13:27:53 -0600

    adds aliases and du

--- Powershell_profile.ps1
+++ Powershell_profile.ps1
@@ -1,16 +1,27 @@
 function grep($strSearch)
 {
-	$input | Where-Object { $_.Name -like "*$strSearch*" }
+    $input | Where-Object { $_.Name -like "*$strSearch*" }
 }
 
 function greps($strSearch)
 {
-	$input | Foreach-Object { 
-		$strC = $_ | Out-String -Stream
-		#$strC;
-		if ($strC -like "*$strSearch*")
-		{
-			$strC;
-		}
-	}
+    $input | Foreach-Object { 
+        $strC = $_ | Out-String -Stream
+        #$strC;
+        if ($strC -like "*$strSearch*")
+        {
+            $strC;
+        }
+    }
 }
+
+function du()
+{
+    $totalLength = Get-ChildItem -Recurse | Measure-Object -Property Length -Sum
+    "Length = {0:N6}" -f ($totalLength.Sum/1MB) + " MB"
+}
+
+Set-Alias o Invoke-Item
+Set-Alias open Invoke-Item
+Set-Alias edit sublime_text
+Set-Alias ll Get-ChildItem
